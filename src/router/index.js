270f3b271c14770b70739e82bbae33588d69a07c
dongmin kim <emthrology@gmail.com>
2021-12-21 13:58:00 +0900

    router code-splitting

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,32 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+// import LoginPage from '@/views/LoginPage.vue';
+// import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
 
+//코드 스플리팅
+//SPA 초기 로드 부하 방지 lazy loading 통해서 -> 위와같이 static import 안함
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/LoginPage.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/signup',
+    name: 'Signup',
+    component: () => import('@/views/SignupPage.vue'),
   },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
 ];
 
 const router = new VueRouter({
