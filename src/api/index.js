e98920ffa39201d5aad577e3afa6c333104cd519
dongmin kim <emthrology@gmail.com>
2021-12-22 10:55:56 +0900

    vue-til axios.create() 생성

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+//axios.create(axiosConfigObject)
+const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+});
+
 function registerUser(data) {
-  const url = 'http://localhost:3000/signup';
-  return axios.post(url, data);
+  // const url = 'http://localhost:3000/signup';
+  // return axios.post(url, data);
+  return instance.post('/signup', data);
 }
 
 export { registerUser };
