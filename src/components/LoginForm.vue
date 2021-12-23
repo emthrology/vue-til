9082129969c4f01b8a0d7d586ccfc6ca11ba506c
dongmin kim <emthrology@gmail.com>
2021-12-23 14:21:52 +0900

    main simple vuex example

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -52,6 +52,7 @@ export default {
         const { data } = await loginUser(userData);
         console.log({ data });
         // alert(`${data.user.username}님 로그인`);
+        this.$store.commit('setUsername', data.user.username);
         this.initForm();
         //vue-router: progmmatic navigation
         this.$router.push('/main');
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,14 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    username: '',
+  },
+  mutations: {
+    setUsername(state, username) {
+      state.username = username;
+    },
+  },
   actions: {},
   modules: {},
 });
