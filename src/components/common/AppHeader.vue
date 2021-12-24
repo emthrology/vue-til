e6957f24f0a73872728ca0870feb9fd90168df36
dongmin kim <emthrology@gmail.com>
2021-12-24 10:44:12 +0900

    vue-til appheader 18

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <header>
     <div>
-      <router-link to="/" class="logo"> TIL </router-link>
+      <router-link to="/" class="logo">
+        TIL
+        <span v-if="isLogin" class="username">{{ $store.state.username }}</span>
+      </router-link>
     </div>
     <div class="navigations">
       <template v-if="isLogin">
-        <span class="username">{{ $store.state.username }}</span>
         &nbsp;
         <span class="logout" @click="logout">logout</span>
       </template>
