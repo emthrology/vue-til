22b3f5c012c446a14841d3027a9b1871b68f9603
dongmin kim <emthrology@gmail.com>
2021-12-27 10:56:21 +0900

    postPage createPost api connected

--- src/components/posts/PostAddForm.vue
+++ src/components/posts/PostAddForm.vue
@@ -5,11 +5,11 @@
       <form class="form" @submit.prevent="submitForm">
         <div>
           <label for="title">Title: </label>
-          <input id="title" type="text" v-model="title" />
+          <input id="title" type="text" v-ime-model="title" />
         </div>
         <div>
           <label for="contents">Contents: </label>
-          <textarea id="contents" rows="5" v-model="contents" />
+          <textarea id="contents" rows="5" v-ime-model="contents" />
         </div>
         <button class="btn" type="submit">Create</button>
       </form>
@@ -18,12 +18,26 @@
 </template>
 
 <script>
+import { createPost } from '@/api';
 export default {
   data() {
     return {
       title: '',
       contents: '',
     };
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const response = await createPost({
+          title: this.title,
+          contents: this.contents,
+        });
+        console.log(response);
+      } catch (error) {
+        alert(error);
+      }
+    },
   },
 };
 </script>
