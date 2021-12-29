cd4c5c3f91d4f75b12501e7c7e982993e3485318
dongmin kim <emthrology@gmail.com>
2021-12-29 14:32:35 +0900

    14 shallowMount wrapper 에서 computed 요소 접근

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -7,12 +7,14 @@ describe('LoginForm.vue', ()=> {
         const wrapper = shallowMount(LoginForm, {
             data() {
                 return {
-                    username: 'test'
+                    username: 'test@adj.com'
                 }
             },
         });
         const usernameInput = wrapper.find('#username');
         console.log(usernameInput.element.value);
+        console.log(wrapper.vm.isUsernameValid); //컴포넌트 computed 접근
+
     })
     // test('username should be initialized when component is mounted', () => {
     //     const wrapper = shallowMount(LoginForm);
