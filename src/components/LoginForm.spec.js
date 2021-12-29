5afae14cedf3758a6b76b05da4228a07466b61eb
dongmin kim <emthrology@gmail.com>
2021-12-29 14:39:52 +0900

    14 사용자 경험에 기반한 테스트 코드

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -7,13 +7,16 @@ describe('LoginForm.vue', ()=> {
         const wrapper = shallowMount(LoginForm, {
             data() {
                 return {
-                    username: 'test@adj.com'
+                    username: 'malformedUsername'
                 }
             },
         });
-        const usernameInput = wrapper.find('#username');
-        console.log(usernameInput.element.value);
-        console.log(wrapper.vm.isUsernameValid); //컴포넌트 computed 접근
+        //사용자 관점에서, username이 email-form이 아니면 
+        //빨간 글씨로 경고문구가 뜨는 것을 그대로 expect해야
+        //더 precise한 테스트라 할 수 있음.
+        const warningText = wrapper.find('.warning');
+        // console.log(warningText.html());
+        expect(warningText.exists()).toBeTruthy();
 
     })
     // test('username should be initialized when component is mounted', () => {
