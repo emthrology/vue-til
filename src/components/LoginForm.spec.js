f46328041f23fa513aec43f91a7a95c0037b21c2
dongmin kim <emthrology@gmail.com>
2021-12-29 14:28:10 +0900

    14 shallowMoounnt 두번째 파라미터 - dataObject

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -1,12 +1,18 @@
 import { shallowMount } from '@vue/test-utils';
-import Vue from 'vue';
+// import Vue from 'vue';
 import LoginForm from '@/components/LoginForm.vue';
 
 describe('LoginForm.vue', ()=> {
     test('username should be email-formed', () => {
-        const wrapper = shallowMount(LoginForm);
+        const wrapper = shallowMount(LoginForm, {
+            data() {
+                return {
+                    username: 'test'
+                }
+            },
+        });
         const usernameInput = wrapper.find('#username');
-        console.log(usernameInput.html());
+        console.log(usernameInput.element.value);
     })
     // test('username should be initialized when component is mounted', () => {
     //     const wrapper = shallowMount(LoginForm);
