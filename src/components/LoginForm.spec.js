8ad9465569d2c7431dbe548c1e44c75a4b6368f8
dongmin kim <emthrology@gmail.com>
2021-12-29 14:18:47 +0900

    14 vue-test-utils shallowMount 사용법

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -1,10 +1,12 @@
+import { shallowMount } from '@vue/test-utils';
 import Vue from 'vue';
 import LoginForm from '@/components/LoginForm.vue';
 
 describe('LoginForm.vue', ()=> {
     test('username should be initialized when component is mounted', () => {
-        const wrapper = new Vue(LoginForm).$mount();
-        console.log(wrapper.username);
-        expect(wrapper.username).toBe('');
+        // const wrapper = new Vue(LoginForm).$mount();
+        const wrapper = shallowMount(LoginForm);
+        // expect(wrapper.username).toBe('');
+        expect(wrapper.vm.username).toBe('');
     })
 })
