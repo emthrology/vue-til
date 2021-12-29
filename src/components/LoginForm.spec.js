73056cf3dcca6799f0e132788513c4231f96de6b
dongmin kim <emthrology@gmail.com>
2021-12-29 14:23:23 +0900

    14 mount된 component의 html 요소 특정하기

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -3,10 +3,13 @@ import Vue from 'vue';
 import LoginForm from '@/components/LoginForm.vue';
 
 describe('LoginForm.vue', ()=> {
-    test('username should be initialized when component is mounted', () => {
-        // const wrapper = new Vue(LoginForm).$mount();
+    test('username should be email-formed', () => {
         const wrapper = shallowMount(LoginForm);
-        // expect(wrapper.username).toBe('');
-        expect(wrapper.vm.username).toBe('');
+        const usernameInput = wrapper.find('#username');
+        console.log(usernameInput.html());
     })
+    // test('username should be initialized when component is mounted', () => {
+    //     const wrapper = shallowMount(LoginForm);
+    //     expect(wrapper.vm.username).toBe('');
+    // })
 })
